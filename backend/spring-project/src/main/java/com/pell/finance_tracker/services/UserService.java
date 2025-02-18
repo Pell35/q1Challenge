7ac7df360575d52f627c9b36b2f352e80fb78b30
Pell35 <pjelliott11@gmail.com>
2025-02-18 00:03:21 -0600

    Update UserService to register a new user, encode passwords, and find by username. Update SecurityConfig to help encode passwords so they're not stored in plain text.

--- backend/spring-project/src/main/java/com/pell/finance_tracker/services/UserService.java
+++ backend/spring-project/src/main/java/com/pell/finance_tracker/services/UserService.java
@@ -9,5 +9,23 @@ import java.util.Optional;
 
 @Service
 public class UserService{
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository =userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+    public User registerUser(String username, String password) {
+        if (userRepository.findByUsername(username).isPresent()) {
+            throw new RuntimeException ("Username already exists");
+        }
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(passwordEncoder.encode(password));
+        return userRepository.save(user);
+    }
+    public Optional<User> findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
 }
