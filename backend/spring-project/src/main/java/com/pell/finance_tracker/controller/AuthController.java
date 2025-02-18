413d6cfa28d6975bf41b4d8b2a74cc5bb7735466
Pell35 <pjelliott11@gmail.com>
2025-02-17 23:46:06 -0600

    Update authController for mapping to auth or signup

--- backend/spring-project/src/main/java/com/pell/finance_tracker/controller/AuthController.java
+++ backend/spring-project/src/main/java/com/pell/finance_tracker/controller/AuthController.java
@@ -4,3 +4,21 @@ import com.pell.services.UserService;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    private final UserService userService;
+    public AuthController(UserService userService){
+        this.userService = userService;
+    }
+    @PostMapping("/signup")
+    public ResponseEntity<String> registerUser(@RequestParam String username, @RequestParam String password) {
+        try{
+            userService,registerUser(username, password);
+            return ResponseEntity.ok("User registered successfully!");
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+}
