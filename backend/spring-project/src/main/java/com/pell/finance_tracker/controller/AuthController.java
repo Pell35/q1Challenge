5e011c7e484dd5638c758680990a3a9510309c00
Pell35 <pjelliott11@gmail.com>
2025-02-17 23:40:53 -0600

    Created files for authControl, Security config, and UserService to handle logic. Added import statements for later use.

---
+++ backend/spring-project/src/main/java/com/pell/finance_tracker/controller/AuthController.java
@@ -0,0 +1,6 @@
+package com.pell.controllers;
+import com.pell.models.User;
+import com.pell.services.UserService;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
---
+++ backend/spring-project/src/main/java/com/pell/finance_tracker/services/UserService.java
@@ -0,0 +1,13 @@
+package com.pell.services;
+
+import com.pell.models.User;
+import com.pell.repositories.UserRepository;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import java.util.Optional;
+
+@Service
+public class UserService{
+
+}
